7fee99967db26ebd81886489d58ebf288b33e5cb
Madhurjya <migom6@gmail.com>
2018-10-17 12:04:09 +0530

    fetch apimedic api key from environment

--- backend/utils/generatetoken.py
+++ backend/utils/generatetoken.py
@@ -1,12 +1,15 @@
 import requests
+import os
+
 
 def generatetoken():
-  x = "Bearer Mk9r2_GMAIL_COM_AUT:CRdGgfb4bAihZ1tmm64gjA=="
+    x = f"Bearer {os.env.get('APIMEDIC_API_KEY')}"
 
-  headers = {
-    'Content-Type':'application/json',
-    "Authorization": x
-  }
-  response = requests.post('https://authservice.priaid.ch/login', headers=headers)
-  token = response.json()["Token"]
-  return token
+    headers = {
+        'Content-Type': 'application/json',
+        "Authorization": x
+    }
+    response = requests.post(
+        'https://authservice.priaid.ch/login', headers=headers)
+    token = response.json()["Token"]
+    return token
